6bc90b8b1fb0e4895172dcb146e2409aff235641
Paweł Gargula <pawel.gargula@interia.pl>
2023-07-13 12:07:43 +0200

    Update steps

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -6,7 +6,8 @@ export default function Steps() {
                 <li>Create excel file with assets' data</li>
                 <li>Import excel file by Web App</li>
                 <li>Add asset manually by Web App</li>
-                <li>Assets Labeling</li>
+                <li>Print labels for assets</li>
+                <li>Label assets</li>
                 <li>Inventory Creation - Name, Area, Team</li>
                 <li>Inventory by Android app</li>
                 <li>Inventory by Web app</li>
